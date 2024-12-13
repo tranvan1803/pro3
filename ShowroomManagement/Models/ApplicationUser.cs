3a3a25c214c84e88fffeb3ee9e51cf27959ed774
dylandpham <phamdat301005@gmail.com>
2024-12-13 16:36:12 +0700

    first commit

--- ShowroomManagement/Models/ApplicationUser.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-public class ApplicationUser : IdentityUser
-{
-    public string Avatar { get; set; } = "/images/default-avatar.png"; // Đường dẫn mặc định
-}
--- ShowroomManagement/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ShowroomManagement.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
